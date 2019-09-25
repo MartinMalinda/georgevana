3b3012014a5b1f70e012212f147f8180028b79f9
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 13:34:24 +0200

    week03day03 Blog post exercise

---
+++ week03/Day03/Blog Post/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog_Post
+{
+    public class BlogPost
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PublicationDate { get; set; }
+
+        public BlogPost(string authorName, string title, string text, string publicationDate)
+        {
+            this.AuthorName = authorName;
+            this.Title = title;
+            this.Text = text;
+            this.PublicationDate = publicationDate;
+        }
+    }
+}
---
+++ week03/Day03/Blog Post/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blog_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogPost BlogPostOne = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2005.05.04");
+            BlogPost BlogPostTwo = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10");
+            BlogPost BlogPostThree = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.08");
+        }
+    }
+}
