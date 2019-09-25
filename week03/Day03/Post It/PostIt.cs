f4df3de0360553f2928168bc9ddf0559e5b06406
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 13:05:43 +0200

    week03day03 Post it exercise

---
+++ week03/Day03/Post It/PostIt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Post_It
+{
+    public class PostIt
+    {
+        public string BackgroundColor { get; set; }
+        public string Text { get; set; }
+        public string TextColor { get; set; }
+
+        public PostIt(string background, string text, string textColor)
+        {
+            this.BackgroundColor = background;
+            this.Text = text;
+            this.TextColor = textColor;
+        }
+    }
+}
---
+++ week03/Day03/Post It/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Post_It
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a PostIt a struct that has
+                a BackgroundColor
+                a Text on it
+                a TextColor
+            Create a few example post-it objects:
+                an orange with blue text: "Idea 1"
+                a pink with black text: "Awesome"
+                a yellow with green text: "Superb!"*/
+
+            PostIt PostItOne = new PostIt("Orange", "Idea1", "Blue");
+            PostIt PostItTwo = new PostIt("Pink", "Awesome", "Black");
+            PostIt PostItThree = new PostIt("Yellow", "Superb", "Green");
+
+        }
+    }
+}
