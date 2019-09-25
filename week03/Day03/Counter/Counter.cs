7a8dc9678d77dde8416a3ee86c2f729a87cc1f8d
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 15:01:59 +0200

    week03day03 Counter exercise

---
+++ week03/Day03/Counter/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter
+{
+    public class Counter
+    {
+        public int Number { get; set; } = 0;
+
+        public Counter()
+        {
+            this.Number = default;
+        }
+        public Counter(int number)
+        {
+            this.Number = number;
+        }
+        public void Add()
+        {
+            Number++;
+        }
+        public string Get()
+        {
+            return Convert.ToString(Number);
+        }
+        public void Reset()
+        {
+            this.Number = default;
+        }
+    }
+}
---
+++ week03/Day03/Counter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counterOne = new Counter(50);
+            Console.WriteLine(counterOne.Get());
+            counterOne.Reset();
+            Console.WriteLine(counterOne.Get());
+        }
+    }
+}
