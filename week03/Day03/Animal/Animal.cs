80c6d3bfe1e7fff2fa0b1166ebe275f1bec49af0
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 14:03:28 +0200

    week03day03 Animal exercise

---
+++ week03/Day03/Animal/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    public class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+
+
+        public Animal(int hunger, int thirst)
+        {
+            this.Hunger = hunger;
+            this.Thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+        public void Drink()
+        {
+            Thirst--;
+        }
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+    }
+}
