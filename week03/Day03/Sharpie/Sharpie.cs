cefab6685b6b237b7a3b5e36d6e4b65c084c7e34
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 14:16:57 +0200

    week03day03 Sharpie exercise

---
+++ week03/Day03/Sharpie/Sharpie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    public class Sharpie
+    {
+        public string Color { get; set; }
+        public double Width { get; set; }
+        public double InkAmount { get; set; } = 100;
+
+        public Sharpie(string color, double width)
+        {
+            this.Color = color;
+            this.Width = width;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+    }
+}
