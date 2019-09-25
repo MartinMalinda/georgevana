bf71137b4cf3685f53a42e4b8ee8180a6072e56d
Jiri Vana <georgevanaa@gmail.com>
2019-09-25 17:00:15 +0200

    week03day03 Pokemon exercise

---
+++ week03/Day03/Pokemon/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pokemon
+{
+    public enum PokemonType
+    {
+        Leaf,
+        Electric,
+        Fire,
+        Water
+    }
+
+    class Pokemon
+    {
+        public string Name { get; private set; }
+        public PokemonType Type { get; private set; }
+        public PokemonType EffectiveAgainst { get; private set; }
+
+        public Pokemon(string name, PokemonType type, PokemonType effectiveAgainst)
+        {
+            Name = name;
+            Type = type;
+            EffectiveAgainst = effectiveAgainst;
+        }
+
+        public bool IsEffectiveAgainst(Pokemon anotherPokemon)
+        {
+            return EffectiveAgainst == anotherPokemon.Type;
+        }
+    }
+}
