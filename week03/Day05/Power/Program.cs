0be1f0c7690ef72fa66c9baecdb0819377696514
Jiri Vana <georgevanaa@gmail.com>
2019-09-27 13:43:54 +0200

    week03day05 Sumdigit exercise

---
+++ week03/Day05/Power/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively(no loops) the value of base to the n power, so powerN(3, 2) is 9(3 squared).
+
+            Console.WriteLine("Hello World!");
+        }
+        public static 
+    }
+}
---
+++ week03/Day05/Sum Digits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a non - negative integer n, return the sum of its digits recursively(without loops).
+            // Mod(%) by 10 yields the rightmost digit(e.g. 126 % 10 is 6)
+            // Divide(/) by 10 removes the rightmost digit(e.g. 126 / 10 is 12).
+
+            Console.WriteLine(SumDigits(246810));
+            // result is: 21
+        }
+        public static int SumDigits(int n)
+        {
+            int nLength = Convert.ToString(n).Length;
+            if (nLength == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return (n % 10) + SumDigits(n / 10);
+            }
+        }
+    }
+}
