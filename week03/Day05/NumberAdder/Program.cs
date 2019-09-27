a2903b6dedd15bf1ffaa4fa3dfcb636403abcb05
Jiri Vana <georgevanaa@gmail.com>
2019-09-27 13:10:34 +0200

    week03day05 update

---
+++ week03/Day05/NumberAdder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");
+            Console.WriteLine(AddNumbers(Convert.ToInt32(Console.ReadLine())));
+        }
+        public static int AddNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + AddNumbers(n - 1);
+            }
+        }
+    }
+}
