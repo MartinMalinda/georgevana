9ae70712895bb0bdc71b817c70e8a2651a195f25
Jiri Vana <georgevanaa@gmail.com>
2019-09-27 14:03:43 +0200

    week03day05 Power exercise

--- week03/Day05/Power/Program.cs
+++ week03/Day05/Power/Program.cs
@@ -8,8 +8,18 @@ namespace Power
         {
             // Given base and n that are both 1 or more, compute recursively(no loops) the value of base to the n power, so powerN(3, 2) is 9(3 squared).
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Power(3, 50));
         }
-        public static 
+        public static long Power(long n1, long n2)
+        {
+            if (n2 == 1)
+            {
+                return n1;
+            }
+            else
+            {
+                return n1 * Power(n1, n2 - 1);
+            }
+        }
     }
 }
