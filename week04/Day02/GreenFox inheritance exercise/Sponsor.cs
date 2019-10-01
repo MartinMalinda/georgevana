cdcf5a1086305c07f6e3acdb350fb393a5543618
Jiri Vana <georgevanaa@gmail.com>
2019-10-01 17:26:23 +0200

    GreenFox exercise Sponsor update

---
+++ week04/Day02/GreenFox inheritance exercise/Sponsor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFox_inheritance_exercise
+{
+    class Sponsor : Person
+    {
+        public string Company { get; set; } = "Google";
+        public int HiredStudents { get; set; } = 0;
+
+        public Sponsor()
+        {
+
+        }
+        public Sponsor(string name, int age, Gender gender, string company) : base(name, age, gender)
+        {
+            this.Company = company;
+        }
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {PersonGender} who represents {Company} and hired {HiredStudents} students so far.");
+        }
+        public void Hire()
+        {
+            HiredStudents++;
+        }
+        public override void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+    }
+}
