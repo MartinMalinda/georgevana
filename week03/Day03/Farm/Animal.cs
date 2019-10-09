4475201c735e12794c15334c3ffd184c6263baa7
Jiri Vana <georgevanaa@gmail.com>
2019-10-09 17:40:22 +0200

    Farm exercise from week3 done

---
+++ week03/Day03/Farm/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+
+
+        public Animal(int hunger, int thirst)
+        {
+            this.Hunger = hunger;
+            this.Thirst = thirst;
+        }
+
+        public Animal()
+        {
+            this.Hunger = 50;
+            this.Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+        public void Drink()
+        {
+            Thirst--;
+        }
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+    }
+}
---
+++ week03/Day03/Farm/TheFarm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Farm
+{
+    class TheFarm
+    {
+        public List<Animal> ListOfAnimals { get; set; }
+        public int FreeSlots { get; set; }
+
+        public TheFarm(int freeSlots)
+        {
+            this.FreeSlots = freeSlots;
+        }
+        public void Breed()
+        {
+            if (FreeSlots > 0)
+            {
+                Animal animal = new Animal();
+                ListOfAnimals.Add(animal);
+                FreeSlots--;
+            }
+            else
+            {
+                Console.WriteLine("No free slots");
+            }
+        }
+
+        public void SlaughterOne()
+        {
+            int hunger = 50;
+            int index = 0;
+
+            for (int i = 0; i < ListOfAnimals.Count; i++)
+            {
+                if (ListOfAnimals[i].Hunger <= hunger)
+                {
+                    hunger = ListOfAnimals[i].Hunger;
+                    index = i;
+                }
+            }
+            ListOfAnimals.Remove(ListOfAnimals[index]);
+        }
+
+        public void SlaughterLinq()
+        {
+            var hungryAnimal = ListOfAnimals.OrderByDescending(a => a.Hunger).First();
+            ListOfAnimals.Remove(hungryAnimal);
+        }
+    }
+}
