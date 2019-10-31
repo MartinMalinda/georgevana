c1c9644d31ef580e00c63be19edd81f6af7af5f6
Jiri Vana <georgevanaa@gmail.com>
2019-10-31 11:48:51 +0100

    week7 updates

---
+++ week07/day03/Hello DI project/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_DI_project
+{
+    public class ConsoleLoggerMiddleware : IMiddleware
+    {
+        public Printer printer;
+
+        public ConsoleLoggerMiddleware(Printer printer)
+        {
+            this.printer = printer;
+        }
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hi from the middleware");
+            printer.Log("hello");
+            return next(context);
+        }
+    }
+}
