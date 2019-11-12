e271fd7442cff2c9c320fb7779ab2b4d50f16197
Jiri Vana <georgevanaa@gmail.com>
2019-11-12 13:12:02 +0100

    REST Doubling exercise done

---
+++ week09/day02/REST/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REST.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet("/doubling")]
+        public IActionResult Double(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            else
+            {
+                return Json(new { received = input, result = input * 2 });
+            }
+        }
+    }
+}
