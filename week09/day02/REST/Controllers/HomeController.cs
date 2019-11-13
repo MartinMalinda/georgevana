e4448e8a811d91affff78d4adb4e07317c7de25f
Jiri Vana <georgevanaa@gmail.com>
2019-11-13 16:10:59 +0100

    REST Arrays exercise done

--- week09/day02/REST/Controllers/HomeController.cs
+++ week09/day02/REST/Controllers/HomeController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using REST.Models;
 
 namespace REST.Controllers
 {
     [Route("/")]
+    [ApiController]
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -16,7 +18,7 @@ namespace REST.Controllers
         }
 
         [HttpGet("/doubling")]
-        public IActionResult Double(int? input)
+        public ActionResult Double(int? input)
         {
             if (input == null)
             {
@@ -29,7 +31,7 @@ namespace REST.Controllers
         }
 
         [HttpGet("/greeter")]
-        public IActionResult Greet(string name, string title)
+        public ActionResult Greet(string name, string title)
         {
             if (string.IsNullOrEmpty(name) && title == null)
             {
@@ -50,20 +52,24 @@ namespace REST.Controllers
         }
 
         [HttpGet("/appenda/{appendable}")]
-        public IActionResult AppendA([FromRoute] string appendable)
+        public ActionResult AppendA([FromRoute] string appendable)
         {
             return Json(new { appended = $"{appendable}a" });
         }
 
         [HttpPost("/dountil/{operation}")]
-        public IActionResult DoUntil([FromRoute] string operation, [FromBody]JObject until)
+        public ActionResult DoUntil([FromRoute] string operation, [FromBody]MathRequest request)
         {
-            if (until != null)
+            if (request == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+            else
             {
                 if (operation == "sum")
                 {
                     int result = 0;
-                    for (int j = 0; j <= until.Value<int>("until"); j++)
+                    for (int j = 0; j <= request.Limit; j++)
                     {
                         result += j;
                     }
@@ -72,14 +78,40 @@ namespace REST.Controllers
                 if (operation == "factor")
                 {
                     int result = 1;
-                    for (int i = 1; i <= until.Value<int>("until"); i++)
+                    for (int i = 1; i <= request.Limit; i++)
                     {
                         result *= i;
                     }
                     return Json(new { result });
                 }
             }
-            return Json(new { error = "Please provide a number!" });
+            return Ok();
+        }
+
+        [HttpPost("/arrays")]
+        public ActionResult DoMath([FromBody]MathRequest request)
+        {
+            if (request == null)
+            {
+                return Json(new { error = "Please provide what to do with the numbers!" });
+            }
+            else
+            {
+                object result = null;
+                switch (request.Operation)
+                {
+                    case "sum":
+                        result = request.Numbers.Sum();
+                        break;
+                    case "multiply":
+                        result = request.Numbers.Aggregate((a, b)=> a * b);
+                        break;
+                    case "double":
+                        result = request.Numbers.Select(n => n * 2).ToArray();
+                        break;
+                }
+                return Ok(new { result });
+            }
         }
     }
 }
---
+++ week09/day02/REST/Models/MathRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Models
+{
+    public class MathRequest
+    {
+        [JsonProperty("operation")]
+        public string Operation { get; set; }
+        [JsonProperty("numbers")]
+        public int[] Numbers { get; set; }
+        [JsonProperty("until")]
+        public int Limit { get; set; }
+    }
+}
